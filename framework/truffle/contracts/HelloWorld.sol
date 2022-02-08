c3bc0dd10597e68e8d854eb6b0c8de5af6977fc5
designerasun <asuntheawesome@gmail.com>
2022-02-08 23:53:03 +0900

    feat : adding HelloWorld message contracts

--- framework/truffle/contracts/HelloWorld.sol
+++ framework/truffle/contracts/HelloWorld.sol
@@ -2,7 +2,20 @@
 pragma solidity ^0.8.10;
 
 contract HelloWorld {
-    function getHelloWorld() public pure returns (string memory) {
-        return "Hello World";
+    string public message;
+    address owner;
+    
+    constructor(string memory _message) {
+        message = _message; 
+        owner = msg.sender; // set contract invoker to owner
     }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public payable {
+        require(msg.value >= 1 ether, "message fee costs 1 ether");
+        message = _message; 
+    } 
 }
---
+++ framework/truffle/contracts/HelloWorldClone.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "../../../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract HelloWorldClone {
+    // import library Strings. Strings.sol is imported in ERC721Enumerable.sol
+    using Strings for uint256; 
+    bytes32 public constant name = "jake";
+    uint256 public wow = 3; 
+    string public test = wow.toString();
+}
