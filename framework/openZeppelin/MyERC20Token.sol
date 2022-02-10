4bc85e06d4222c2759ac467b33bb7a18ca9737ce
designerasun <asuntheawesome@gmail.com>
2022-02-10 22:42:42 +0900

    feat : ERC20 token with supply/pausability

---
+++ framework/openZeppelin/MyERC20Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "./node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+// match file name and contract name(convention, gas tip)
+contract MyERC20Token is ERC20, ERC20Capped, ERC20Pausable {
+
+    // ERC20Capped(1000) => immutable, set token supply to 1000, set during construction time
+    constructor()ERC20("MyERC20", "MYERC20Symbol") ERC20Capped(1000){}
+
+    // destroy some amount of tokens
+    function burn(address _to, uint256 _amount) public {
+        _burn(_to, _amount);
+    }
+
+    // _mint should be overriden :  ERC20, ERC20Capped 
+    function _mint(address _to, uint256 _amount) internal override(ERC20, ERC20Capped)  {
+        _mint(_to, _amount);
+    }
+    
+    // _beforeTokenTransfer should be overriden :  ERC20, ERC20Capped 
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+        require(true, "ERC20Pausable: token transfer while paused");
+    }
+
+}
