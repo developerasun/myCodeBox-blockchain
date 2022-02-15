248bc065b75c83526eda932d540d9f07fbf71297
designerasun <asuntheawesome@gmail.com>
2022-02-15 13:18:36 +0900

    feat : call getter/setter in contract with web3

---
+++ web3/interactWithContract/getterSetter/contracts/MyContract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract MyContract {
+    uint256 data; 
+
+    function getData() external view returns(uint256) {
+        return data;
+    }
+
+    function setData(uint256 _data) external {
+        data = _data; 
+    }
+
+    function setDataPrivate(uint256 _data) private {
+        data = _data + 10;
+    }
+}
---
+++ web3/interactWithContract/getterSetter/index.js
@@ -0,0 +1,31 @@
+const Web3 = require('web3')
+const PROVIDER = require('../../config/key.js')
+const myContract = require('./build/contracts/MyContract.json')
+
+const init = async () => {
+    try {
+        const web3 = new Web3(PROVIDER.PROVIDER.GANACHE)
+        const id = await web3.eth.net.getId() 
+        const deployedNetwork = myContract.networks[id]
+
+        // create a contract instance
+        const contract = new web3.eth.Contract(myContract.abi, deployedNetwork.address)
+    
+        // get accounts from provider
+        const addresses = await web3.eth.getAccounts()
+        console.log(addresses[0]) // first account of GANACHE
+
+        // call setData method in the contract
+        await contract.methods.setData(10).send({
+            from : addresses[0],
+        })
+        
+        // call getData method in the contract
+        const data = await contract.methods.getData().call()
+        console.log(data)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+init()
