49f9990d0b0d45b58571130dcf2df66d7b8b5372
designerasun <asuntheawesome@gmail.com>
2022-03-18 16:27:39 +0900

    feat: add JoinHandle for thread

--- rust/basic/src/my_thread.rs
+++ rust/basic/src/my_thread.rs
@@ -12,3 +12,31 @@ pub mod create_thread {
         });
     }
 }
+
+pub mod create_join {
+    pub fn wait_to_finish() {
+        let handle = super::thread::spawn(|| {
+            for i in 1..10 {
+                println!("from spawned thread : {}", i);
+                super::thread::sleep(super::Duration::from_millis(1));
+            }
+        });
+        handle.join().unwrap();
+    }
+}
+// // join : Calling join on the handle blocks the thread currently running, 
+// // waiting for the associated thread to finish.
+// let handle = thread::spawn(|| {
+//     for i in 1..10 {
+//         println!("from spawned thread : {}", i);
+//         thread::sleep(Duration::from_millis(1));
+//     }
+// });
+
+// in main thread
+// for i in 1..5 {
+//     println!("{}", i);
+//     thread::sleep(Duration::from_millis(1));
+// }
+
+// handle.join().unwrap();
