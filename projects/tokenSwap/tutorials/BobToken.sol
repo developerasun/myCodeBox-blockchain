601e11c115c2dc7c86ad2ee0274a836a2db8d2a9
designerasun <asuntheawesome@gmail.com>
2022-05-28 18:33:15 +0900

    feat: add swap contract and simple ERC20 tokens

---
+++ projects/tokenSwap/tutorials/BobToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BobToken is ERC20 {
+    constructor() ERC20("BOB", "B"){
+        _mint(msg.sender, 100 * uint256(10 * decimals()));
+    }
+}
