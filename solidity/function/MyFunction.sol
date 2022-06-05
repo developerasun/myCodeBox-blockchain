6aeec272b51f510f5c8192efcf6b2d4493f82a17
designerasun <asuntheawesome@gmail.com>
2022-06-05 21:11:04 +0900

    ENH: add function selector and msg.data example

--- solidity/function/MyFunction.sol
+++ solidity/function/MyFunction.sol
@@ -35,3 +35,27 @@ contract MyFunction {
         return (8, "hello"); // wrap return values with parenthesis
     }
 }
+
+contract MySelector {
+    event Log(bytes data);
+
+    function transfer(address _to, uint _amount) external {
+        emit Log(msg.data);
+                // msg.data is divided as follows
+        // 1) 0xa9059cbb: a function selector, the first four bytes encode the function to call
+        // 2) 000000000000000000000000ab8483f64d9c6d1ecf9b849ae677dd3315835cb2: the value of param 1 address(_to)
+        // the address value copied from Remix: 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+        // 3) 000000000000000000000000000000000000000000000000000000000000000b: the value of param 2 uint(_amount)
+        // the amount value entered by user: 11
+        // : function parameters to pass
+    }
+}
+
+contract FunctionSelector {
+    // example: "transfer(address,uint256)"
+    // note that enter no white space between parameters(hash value changes)
+    function GetSelector(string calldata functionSignature) external pure returns (bytes4) {
+        // string => dynamic byte array => bytes32 ====(truncating)====> bytes4
+        return bytes4(keccak256(bytes(functionSignature)));
+    }
+}
