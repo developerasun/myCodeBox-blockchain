93839325fa9f90b7e90d9a118773da062d75cde2
designerasun <asuntheawesome@gmail.com>
2022-06-05 21:11:56 +0900

    ENH: set logging func for playground env

--- playground/playground.js
+++ playground/playground.js
@@ -1,3 +1,9 @@
-if (window.web3.currentProvider) { 
-    console.log("provider exists")
+function LogItemLength(item, value) {
+    if (typeof(value) == "string" && typeof(item) == "string") console.log(`the ${item} length: ${value.length}`)    
+    else console.log("enter params in string")
 }
+
+console.log("000000000000000000000000".length)
+
+LogItemLength("private key", "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd036415f")
+LogItemLength("contract account", "0x06012c8cf97bead5deae237070f9587f8e7a266d")
