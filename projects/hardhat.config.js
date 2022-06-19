9f1cbc9c42e218badf09d8f557c1bca0b5de3828
designerasun <asuntheawesome@gmail.com>
2022-06-19 23:08:42 +0900

    BLD: add a minial hardhat configuration/package.json for mini project

--- projects/hardhat.config.js
+++ projects/hardhat.config.js
@@ -1,23 +1,21 @@
 // require("@nomiclabs/hardhat-waffle");
-require("@openzeppelin/hardhat-upgrades");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config();
+require('@openzeppelin/hardhat-upgrades')
+require('@nomiclabs/hardhat-etherscan')
+require('@nomiclabs/hardhat-ethers')
+require('dotenv').config()
 
-// box contract deployed to: 0x5F8C412BCBF0E8357A3C6Aed51214854a1cFA0D3
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.14",
-  networks: {
-    ropsten: {
-      url: process.env.INFURA_API_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+    solidity: '0.8.15',
+    networks: {
+        ropsten: {
+            url: process.env.INFURA_API_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        }
     },
-  },
-  etherscan: {
-    apiKey: {
-      ropsten: process.env.ETHERSCAN_API,
-    },
-  },
-};
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API
+    }
+}
