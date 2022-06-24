7be39cd5e5f8cbb48e32d34570f089eb267a1edd
designerasun <asuntheawesome@gmail.com>
2022-06-24 23:06:17 +0900

    ENH: use embedded library in calling contract

--- solidity/library/AddFive.sol
+++ solidity/library/AddFive.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// purpose of using library is to lower gas cost by
-// reusing codes.
+// using library is to lower gas cost by reusing codes.
 library AddFive {
-    // library cannnot have state variales
+    // uint256 noStateVariable; // library cannnot have state variales
     function cal(uint _number) public pure returns(uint) {
         return _number +5;
     }
