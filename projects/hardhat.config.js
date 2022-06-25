37549881aee7aebdd637b8af5ae3c20e6c9ead1b
designerasun <asuntheawesome@gmail.com>
2022-06-25 17:33:33 +0900

    BLD/CLN: add prettier/package manager configs

--- projects/hardhat.config.js
+++ projects/hardhat.config.js
@@ -1,21 +1,21 @@
 // require("@nomiclabs/hardhat-waffle");
-require('@openzeppelin/hardhat-upgrades')
-require('@nomiclabs/hardhat-etherscan')
-require('@nomiclabs/hardhat-ethers')
-require('dotenv').config()
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: '0.8.15',
-    networks: {
-        ropsten: {
-            url: process.env.INFURA_API_KEY,
-            accounts: [process.env.PRIVATE_KEY]
-        }
+  solidity: "0.8.15",
+  networks: {
+    ropsten: {
+      url: process.env.INFURA_API_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
-    etherscan: {
-        apiKey: process.env.ETHERSCAN_API
-    }
-}
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+  },
+};
