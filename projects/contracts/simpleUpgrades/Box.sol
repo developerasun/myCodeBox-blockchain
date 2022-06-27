c16dfe79c1ac142dc74a5423d52f1d42591d4d9c
designerasun <asuntheawesome@gmail.com>
2022-06-28 00:30:51 +0900

    CLN: fix contract version to latest

--- projects/contracts/simpleUpgrades/Box.sol
+++ projects/contracts/simpleUpgrades/Box.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.15;
 
 /// @dev This is a proxy contract that will be deployed very first.
 /// @dev should be verified as proxy in Etherscan to perform read/write
 contract Box {
     uint256 public val;
+    uint256 public val2;
 
     function initialize(uint256 _val) external {
         val = _val;
--- projects/contracts/tokenSwap/BobToken.sol
+++ projects/contracts/tokenSwap/BobToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BobToken is ERC20 {
-    constructor() ERC20("BOB", "B"){
-        _mint(msg.sender, 100 * uint256(10 ** decimals()));
+    constructor() ERC20("BOB", "B") {
+        _mint(msg.sender, 100 * uint256(10**decimals()));
     }
 }
