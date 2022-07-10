9ac8805e43ad56f75df0199f82cf6e91d2d73518
designerasun <asuntheawesome@gmail.com>
2022-07-10 19:51:32 +0900

    TYP:set types.d.ts for block types

---
+++ projects/scripts/blockchain/src/types.d.ts
@@ -0,0 +1,9 @@
+// .d.ts file is used for compiler assistance
+// no function implementation in .d.ts file
+// no .d.ts file extension for file import path
+
+export declare interface BlockProps {
+  index: number;
+  previousHash: string;
+  data: any;
+}
